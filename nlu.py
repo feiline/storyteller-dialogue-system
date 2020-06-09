50b6a2a683ec6fb31f0ff09e145786c59f6efc79
feiIin <sabrina.chiesurin@gmail.com>
2020-06-09 18:50:12 +0100

    nlu.py and nlu_Rasa updated

---
+++ nlu.py
@@ -0,0 +1,15 @@
+from rasa.nlu.model import Interpreter
+
+
+def get_intent(utterance):
+    model = "rasa_nlu/models/nlu"
+
+    interpreter = Interpreter.load(model)
+    interpretation = interpreter.parse(utterance)
+
+    return interpretation
+
+if __name__ == "__main__":
+    utterance = "Hello, world!"
+    intent = get_intent(utterance)
+    print(intent)
