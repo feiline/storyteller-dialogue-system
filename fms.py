7029c0aa7dc0ac925a216adc4b4c740b03185c1b
feiIin <sabrina.chiesurin@gmail.com>
2020-06-13 14:13:04 +0100

    more states and transitions added

--- fms.py
+++ fms.py
@@ -3,15 +3,42 @@ from transitions import Machine
 class Conversation(object):
 
     # define states
-    states = ['introduction', 'storytelling', 'closing']
+    states = ['introduction', 'storytelling', 'closing', 'answering', 'link_to_survey', 'answering_f']
 
     def __init__(self, id):
         self.id = id
         self.machine = Machine(model=self, states=Conversation.states, initial='introduction')
 
-        # transitions
+        # ----------- TRANSITIONS ------------ #
+
+        # ---- Transition from introduction --- #
         self.machine.add_transition('acceptance', 'introduction', 'storytelling')
+        self.machine.add_transition('question', 'introduction', 'answering')
+
+        # ---- Transition from storytelling --- #
         self.machine.add_transition('story_ends', 'storytelling', 'closing')
+        self.machine.add_transition('question', 'storytelling', 'bidaf_answering')
+        self.machine.add_transition('non-questions', 'storytelling', 'closing', conditions='story_ended')
+        self.machine.add_transition('non-questions', 'storytelling', 'storytelling')
+
+        # ---- Transition from answering --- #
+        self.machine.add_transition('acceptance', 'answering', 'introduction')
+        self.machine.add_transition('question', 'answering', 'answering')
+        self.machine.add_transition('ask_increment', 'answering', 'storytelling')
+
+        # ---- Transition from bidaf_answering --- #
+        self.machine.add_transition('non_question', 'bidaf_answering', 'storytelling')
+        self.machine.add_transition('non_question', 'bidaf_answering', 'closing', conditions='story_ended')
+        self.machine.add_transition('question', 'bidaf_answering', 'bidaf_answering')
+
+        # ---- Transition from closing --- #
+        self.machine.add_transition('acceptance', 'closing', 'link_to_survey')
+        self.machine.add_transition('question', 'closing', 'answering_f')
+
+        # ---- Transition from answering_f --- #
+        self.machine.add_transition('question', 'answering_f', 'answering_f')
+
+
 
 if __name__ == "__main__":
     conv1 = Conversation('1')
