031f1f96fa6ed4d11a3cccc0b20b62c15f4e08ef
feiIin <sabrina.chiesurin@gmail.com>
2020-06-15 17:58:43 +0100

    transitions and states added

--- fms.py
+++ fms.py
@@ -1,13 +1,13 @@
 from transitions import Machine
 
-class Conversation(object):
+
+class ConversationFMS(object):
 
     # define states
-    states = ['introduction', 'storytelling', 'closing', 'answering', 'link_to_survey', 'answering_f']
+    states = ['introduction', 'storytelling', 'closing', 'answering', 'link_to_survey', 'answering_f', 'ans_bidaf']
 
-    def __init__(self, id):
-        self.id = id
-        self.machine = Machine(model=self, states=Conversation.states, initial='introduction')
+    def __init__(self, state):
+        self.machine = Machine(model=self, states=ConversationFMS.states, initial=state)
 
         # ----------- TRANSITIONS ------------ #
 
@@ -18,18 +18,18 @@ class Conversation(object):
         # ---- Transition from storytelling --- #
         self.machine.add_transition('story_ends', 'storytelling', 'closing')
         self.machine.add_transition('question', 'storytelling', 'bidaf_answering')
-        self.machine.add_transition('non-questions', 'storytelling', 'closing', conditions='story_ended')
-        self.machine.add_transition('non-questions', 'storytelling', 'storytelling')
+        self.machine.add_transition('non_question', 'storytelling', 'closing', conditions='story_ended')
+        self.machine.add_transition('non_question', 'storytelling', 'storytelling')
 
         # ---- Transition from answering --- #
         self.machine.add_transition('acceptance', 'answering', 'introduction')
         self.machine.add_transition('question', 'answering', 'answering')
         self.machine.add_transition('ask_increment', 'answering', 'storytelling')
 
-        # ---- Transition from bidaf_answering --- #
-        self.machine.add_transition('non_question', 'bidaf_answering', 'storytelling')
-        self.machine.add_transition('non_question', 'bidaf_answering', 'closing', conditions='story_ended')
-        self.machine.add_transition('question', 'bidaf_answering', 'bidaf_answering')
+        # ---- Transition from ans_bidaf --- #
+        self.machine.add_transition('acceptance', 'ans_bidaf', 'storytelling')
+        self.machine.add_transition('acceptance', 'ans_bidaf', 'closing', conditions='story_ended')
+        self.machine.add_transition('question', 'ans_bidaf', 'ans_bidaf')
 
         # ---- Transition from closing --- #
         self.machine.add_transition('acceptance', 'closing', 'link_to_survey')
@@ -37,11 +37,11 @@ class Conversation(object):
 
         # ---- Transition from answering_f --- #
         self.machine.add_transition('question', 'answering_f', 'answering_f')
-
+        self.machine.add_transition('acceptance', 'answering_f', 'link_to_survey')
 
 
 if __name__ == "__main__":
-    conv1 = Conversation('1')
+    conv1 = ConversationFMS('1')
     print("This is the initial state: ", conv1.state)
     conv1.acceptance()
     print("This is the state after 'acceptance' from the user: ", conv1.state)
