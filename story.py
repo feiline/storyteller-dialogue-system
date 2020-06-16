248125a498b9f0e6e316dc84e8fb56268aeab3ef
feiIin <sabrina.chiesurin@gmail.com>
2020-06-16 15:49:00 +0100

    story to use as example, store in a dictionary

---
+++ story.py
@@ -0,0 +1,23 @@
+Original = "This is one of those times I wish I had a digital camera. " \
+           "We keep a large stainless steel bowl of water outside on the back deck for Benjamin to drink out " \
+           "of when he's playing outside. His bowl has become a very popular site. " \
+           "Throughout the day, many birds drink out of it and bathe in it. " \
+           "The birds literally line up on the railing and wait their turn. Squirrels also come to drink out of it. " \
+           "The craziest squirrel just came by- he was literally jumping in fright at what I believe was" \
+           " his own reflection in the bowl. He was startled so much at one point that he leap in the air " \
+           "and fell off the deck. But not quite, I saw his one little paw hanging on! After a moment or two his paw " \
+           "slipped and he tumbled down a few feet. But oh, if you could have seen the look on his startled face " \
+           "and how he jumped back each time he caught his reflection in the bowl!"
+DICT_STORY = {'1': "This is one of those times I wish I had a digital camera.",
+              '2': "We keep a large stainless steel bowl of water outside on the back deck for Benjamin "
+                   "to drink out of when he's playing outside.",
+              '3': "His bowl has become a very popular site. The birds literally line up on the railing and wait "
+                   "their turn.",
+              '4': "Squirrels also come to drink out of it.",
+              '5': "The craziest squirrel just came by- he was literally jumping in fright at what I believe was "
+                   "his own reflection in the bowl",
+              '6': "He was startled so much at one point that he leap in the air and fell off the deck.",
+              '7': "But not quite, I saw his one little paw hanging on!",
+              '8': "After a moment or two his paw slipped and he tumbled down a few feet.",
+              '9': "But oh, if you could have seen the look on his startled face and how he jumped back each time "
+                   "he caught his reflection in the bowl!"}
