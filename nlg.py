c240320d47b7aaae78c10a5b6691f0263a98cc3e
feiIin <sabrina.chiesurin@gmail.com>
2020-06-16 15:43:04 +0100

    refactoring and intro_template() implemented

--- nlg.py
+++ nlg.py
@@ -2,87 +2,285 @@ import random
 import sys
 import logging
 
-
 logger = logging.getLogger(__name__)
 
-_recsys_templates = [
-    "....I think you should do {}.",
-    ".......{}"
-]
-
-
-# Intent: User preference: -ive
-# Example: (I dont like {})
-_questionaire = [
-    "Welcome back {}. So, what would you like to do?"
-]
-
-
+# --------------- FILL THESE DICTIONARIES WITH TEMPLATES ---------------------- #
 # Intent: User preference: +ive / Linked Concepts
 # Example: (I like {})
-_driver_templates = {
-    "x_intent": {
-        # prop - artist
-        # entity - band
-        # linked entity - another artist
-        "a_entity": [
-            "Okay. I guess you know that {entity} is in {quest}. What is {object} that you like to find?"
-        ]
-    }
+
+
+_intro_templates = {
+    "greet": [
+        "Hello! I have a story to tell you",
+        "Hello, are you here for a story?",
+        "Hi, would you like to hear a story?",
+    ],
+    "goodbye": ["Would you like to hear a story?"],
+    "affirm": [ "Ok, here is the story: {text} "],
+    "deny": [  # TODO: write something that makes sense
+        "You can close this session if you wish. Write 'stop' to exit or ' "],
+    "exclaim_neg": [   # TODO: write something that makes sense
+         "You can close this session if you wish. Write 'stop' to exit or ' "],
+    "exclaim_pos": ["Ok, here is the story: {text}"],
+    "thanks": ["Ok, here is the story: {text}"],
+    "ynq": ["Not sure, we'll see."],
+    "whq": ["Not sure, we'll see."],
+    "idk_response": ["Well, just in case, here is the story: {text}"],
+    "request_increment": ["{text}"],
+    "refocus_topic": ["I will tell you a story that I know. Shall I start?"],
+    "clarification_request": ["I will tell you a story that I know. Shall I start?"],
+    "correction": ["I will tell you a story that I know. Shall I start?"],
+    "ask_if_ended": ["It is not started yet. Shall I start?"],
+    "ask_for_story": ["{text}"],
+    "comment": ["Ok, here is the story: {text}"],
+    "feedback_prompt": [
+        "Yes, sorry. are you here for a story?"
+        "I'm here. Would you like to hear a story?"
+    ]
+}
+
+_storytelling_templates = {
+    "greet": [],
+    "goodbye": [],
+    "affirm": [],
+    "deny": [],
+    "exclaim_neg": [],
+    "exclaim_pos": [],
+    "thanks": [],
+    "ynq": [],
+    "whq": [],
+    "idk_response": [],
+    "request_increment": [],
+    "refocus_topic": [],
+    "clarification_request": [],
+    "correction": [],
+    "ask_if_ended": [],
+    "ask_for_story": [],
+    "comment": [],
+    "feedback_prompt": [],
+}
+
+_closing_templates = {
+    "greet": [],
+    "goodbye": [],
+    "affirm": [],
+    "deny": [],
+    "exclaim_neg": [],
+    "exclaim_pos": [],
+    "thanks": [],
+    "ynq": [],
+    "whq": [],
+    "idk_response": [],
+    "request_increment": [],
+    "refocus_topic": [],
+    "clarification_request": [],
+    "correction": [],
+    "ask_if_ended": [],
+    "ask_for_story": [],
+    "comment": [],
+    "feedback_prompt": [],
+}
+
+_answering_templates = {
+    "greet": [],
+    "goodbye": [],
+    "affirm": [],
+    "deny": [],
+    "exclaim_neg": [],
+    "exclaim_pos": [],
+    "thanks": [],
+    "ynq": [],
+    "whq": [],
+    "idk_response": [],
+    "request_increment": [],
+    "refocus_topic": [],
+    "clarification_request": [],
+    "correction": [],
+    "ask_if_ended": [],
+    "ask_for_story": [],
+    "comment": [],
+    "feedback_prompt": [],
+}
+
+_answering_f_templates = {
+    "greet": [],
+    "goodbye": [],
+    "affirm": [],
+    "deny": [],
+    "exclaim_neg": [],
+    "exclaim_pos": [],
+    "thanks": [],
+    "ynq": [],
+    "whq": [],
+    "idk_response": [],
+    "request_increment": [],
+    "refocus_topic": [],
+    "clarification_request": [],
+    "correction": [],
+    "ask_if_ended": [],
+    "ask_for_story": [],
+    "comment": [],
+    "feedback_prompt": [],
+}
+
+_ans_bidaf_templates = {
+    "greet": [],
+    "goodbye": [],
+    "affirm": [],
+    "deny": [],
+    "exclaim_neg": [],
+    "exclaim_pos": [],
+    "thanks": [],
+    "ynq": [],
+    "whq": [],
+    "idk_response": [],
+    "request_increment": [],
+    "refocus_topic": [],
+    "clarification_request": [],
+    "correction": [],
+    "ask_if_ended": [],
+    "ask_for_story": [],
+    "comment": [],
+    "feedback_prompt": [],
+}
+
+_link_to_survey_templates = {
+    "greet": [],
+    "goodbye": [],
+    "affirm": [],
+    "deny": [],
+    "exclaim_neg": [],
+    "exclaim_pos": [],
+    "thanks": [],
+    "ynq": [],
+    "whq": [],
+    "idk_response": [],
+    "request_increment": [],
+    "refocus_topic": [],
+    "clarification_request": [],
+    "correction": [],
+    "ask_if_ended": [],
+    "ask_for_story": [],
+    "comment": [],
+    "feedback_prompt": [],
 }
 
 
 class NLG:
-    def fill_template(self, state_dic):
+    def fill_intro(self, state_dic):
         pass
 
-    def fill_explanations(self, state_dic):
+    def fill_storytelling(self, state_dic):
         pass
 
-    def fill_recommendation(self, state_dic):
+    def fill_closing(self, state_dic):
         pass
 
-    def fill_questionaire(self, state_dic):
+    def fill_answering(self, state_dic):
+        pass
+
+    def fill_answering_f(self, state_dic):
+        pass
+
+    def fill_ans_bidaf(self,state_dic):
+        pass
+
+    def fill_link_to_survey(self, state_dic):
         pass
 
 
 class StorytellerGenerator(NLG):
     def __init__(self, args):
-        logger.info("Creating features from dataset file at %s", args.out_dir)
+        logger.info("Creating features from dataset file at ")
 
-    def fill_template(self, state_dic):
-        templates = _driver_templates
-        template_fillers = {k: v["label"] for k, v in results.items() if type(v) == dict}
-        ## here template_fillers should have "entity", "quest", "object"
-        # whatever required by your fillers
+    # def fill_template(self, state_dic):
+    #     templates = _driver_templates
+    #     template_fillers = {k: v["label"] for k, v in results.items() if type(v) == dict}
+    #     ## here template_fillers should have "entity", "quest", "object"
+    #     # whatever required by your fillers
+    #
+    #     if "greet" in results["intents"] and "introduction" in results["state"]:
+    #         curr_templates = templates["greet"]
+    #         template = random.choice(curr_templates["introduction"])
+    #         return template.format(**template_fillers)
 
-        if "x_intent" in results["intents"]:
-            curr_templates = templates["x_intent"]
-            template = random.choice(curr_templates["a_entity"])
+    def fill_intro(self, state_dic):
+        templates = _intro_templates
+        template_fillers = {'text': state_dic["text"]}
+
+        if "greet" in state_dic["intents"]:
+            curr_templates = templates["greet"]
+            template = random.choice(curr_templates)
+            return template
+        elif "request_increment" in state_dic["intents"]:
+            curr_templates = templates["request_increment"]
+            template = random.choice(curr_templates)
             return template.format(**template_fillers)
         else:
-            logger.info("Cant generated template for: {}".format(results))
+            logger.info("Cant generated template for: {}".format(state_dic))
 
-    def fill_recommendation(self, state_dic):
-        template = random.choice(_recsys_templates)
-        game_play = state_dic["game_play"]
+    def fill_storytelling(self, state_dic):
+        templates = _storytelling_templates
+        if "greet" in state_dic["intents"]:
+            curr_templates = templates["greet"]
+            template = random.choice(curr_templates)
+            return template
+        else:
+            logger.info("Cant generated template for: {}".format(state_dic))
 
-        return template.format(game_play)
+    def fill_closing(self, state_dic):
+        templates = _closing_templates
+        if "greet" in state_dic["intents"]:
+            curr_templates = templates["greet"]
+            template = random.choice(curr_templates)
+            return template
+        else:
+            logger.info("Cant generated template for: {}".format(state_dic))
 
-    def fill_questionaire(self, state_dic):
-        # if dic; use this:
-        # template = questionaire[state_dic["ref_key"]]
-        # if list; use this:
-        template = random.choice(_questionaire)
-        return template.format(state_dic["entity"])
+    def fill_answering(self, state_dic):
+        templates = _answering_templates
+        if "greet" in state_dic["intents"]:
+            curr_templates = templates["greet"]
+            template = random.choice(curr_templates)
+            return template
+        else:
+            logger.info("Cant generated template for: {}".format(state_dic))
+
+    def fill_answering_f(self, state_dic):
+        templates = _answering_f_templates
+        if "greet" in state_dic["intents"]:
+            curr_templates = templates["greet"]
+            template = random.choice(curr_templates)
+            return template
+        else:
+            logger.info("Cant generated template for: {}".format(state_dic))
+
+    def fill_ans_bidaf(self, state_dic):
+        templates = _ans_bidaf_templates
+        if "greet" in state_dic["intents"]:
+            curr_templates = templates["greet"]
+            template = random.choice(curr_templates)
+            return template
+        else:
+            logger.info("Cant generated template for: {}".format(state_dic))
+
+    def fill_link_to_survey(self, state_dic):
+        templates = _link_to_survey_templates
+        if "greet" in state_dic["intents"]:
+            curr_templates = templates["greet"]
+            template = random.choice(curr_templates)
+            return template
+        else:
+            logger.info("Cant generated template for: {}".format(state_dic))
 
 
-if __name__ == "__main__":
-    results = {
-               "intent": {},
-               "entity": {},
-               "quest": {}
-               }
-    drivers = StorytellerGenerator()
-    print(drivers.fill_template(results))
-    # print(drivers.fill_explanations(results))
+
+# if __name__ == "__main__":
+#     results = {
+#         "intent": {},
+#         "entity": {},
+#         "pointer_story": {},
+#         "text": {}
+#     }
+#     drivers = StorytellerGenerator()
+#     print(drivers.fill_template(results))
