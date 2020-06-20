3ca022c9813f0ffa7d1af94d3643439f1881f983
feiIin <sabrina.chiesurin@gmail.com>
2020-06-20 19:34:30 +0100

    state object class refactoring

--- state.py
+++ state.py
@@ -1,7 +1,7 @@
 class State:
     def __init__(self, intent, story_told, previous_intent=None):
         self._intent = intent
-        self._story_told = story_told
+        self._story_told = [story_told]
         self._previous_intent = previous_intent
 
     @property
@@ -16,14 +16,14 @@ class State:
     def previous_intent(self):
         return self._intent
 
-    # setter
-    def set_intent(self, new_intent):
+    @intent.setter
+    def intent(self, new_intent):
         self._intent = new_intent
+
+    @previous_intent.setter
+    def previous_intent(self, new_previous_intent):
+        self._previous_intent = new_previous_intent
 
     def add_increment_told(self, new_story_increment):
         self._story_told.append(new_story_increment)
-
-    def set_previous_intent(self):
-        self._previous_intent = self._intent
-
 
