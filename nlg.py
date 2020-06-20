319fae691917de5740eba9ea58cff22d4dbc5d14
feiIin <sabrina.chiesurin@gmail.com>
2020-06-20 19:33:18 +0100

    cleaned nlg template, now just intro and storytelling done

--- nlg.py
+++ nlg.py
@@ -4,10 +4,6 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-# --------------- FILL THESE DICTIONARIES WITH TEMPLATES ---------------------- #
-# Intent: User preference: +ive / Linked Concepts
-# Example: (I like {})
-
 
 _intro_templates = {
     "greet": [
@@ -23,8 +19,6 @@ _intro_templates = {
          "You can close this session if you wish. Write 'stop' to exit or ' "],
     "exclaim_pos": ["Ok, here is the story: {text}"],
     "thanks": ["Ok, here is the story: {text}"],
-    "ynq": ["Not sure, we'll see."],
-    "whq": ["Not sure, we'll see."],
     "idk_response": ["Well, just in case, here is the story: {text}"],
     "request_increment": ["{text}"],
     "refocus_topic": ["I will tell you a story that I know. Shall I start?"],
@@ -40,130 +34,32 @@ _intro_templates = {
 }
 
 _storytelling_templates = {
-    "greet": [],
-    "goodbye": [],
-    "affirm": [],
-    "deny": [],
-    "exclaim_neg": [],
-    "exclaim_pos": [],
-    "thanks": [],
-    "ynq": [],
-    "whq": [],
-    "idk_response": [],
-    "request_increment": [],
-    "refocus_topic": [],
-    "clarification_request": [],
-    "correction": [],
-    "ask_if_ended": [],
-    "ask_for_story": [],
-    "comment": [],
-    "feedback_prompt": [],
+    "greet": ["{text}"],
+    "goodbye": ["{text}"],
+    "affirm": ["{text}"],
+    "deny": ["{text}"],
+    "exclaim_neg": ["{text}"],
+    "exclaim_pos": ["{text}"],
+    "thanks": ["{text}"],
+    "ynq": ["{text}"],
+    "whq": ["{text}"],
+    "request_increment": ["{text}"],
+    "clarification_request": ["{text}"],
+    "ask_if_ended": ["{text}"],
+    "ask_for_story": ["{text}"],
+    "comment": ["{text}"],
+    "feedback_prompt": ["{text}"],
 }
 
-_closing_templates = {
-    "greet": [],
-    "goodbye": [],
-    "affirm": [],
-    "deny": [],
-    "exclaim_neg": [],
-    "exclaim_pos": [],
-    "thanks": [],
-    "ynq": [],
-    "whq": [],
-    "idk_response": [],
-    "request_increment": [],
-    "refocus_topic": [],
-    "clarification_request": [],
-    "correction": [],
-    "ask_if_ended": [],
-    "ask_for_story": [],
-    "comment": [],
-    "feedback_prompt": [],
-}
+_closing_templates = {}
 
-_answering_templates = {
-    "greet": [],
-    "goodbye": [],
-    "affirm": [],
-    "deny": [],
-    "exclaim_neg": [],
-    "exclaim_pos": [],
-    "thanks": [],
-    "ynq": [],
-    "whq": [],
-    "idk_response": [],
-    "request_increment": [],
-    "refocus_topic": [],
-    "clarification_request": [],
-    "correction": [],
-    "ask_if_ended": [],
-    "ask_for_story": [],
-    "comment": [],
-    "feedback_prompt": [],
-}
+_answering_templates = {}
 
-_answering_f_templates = {
-    "greet": [],
-    "goodbye": [],
-    "affirm": [],
-    "deny": [],
-    "exclaim_neg": [],
-    "exclaim_pos": [],
-    "thanks": [],
-    "ynq": [],
-    "whq": [],
-    "idk_response": [],
-    "request_increment": [],
-    "refocus_topic": [],
-    "clarification_request": [],
-    "correction": [],
-    "ask_if_ended": [],
-    "ask_for_story": [],
-    "comment": [],
-    "feedback_prompt": [],
-}
+_answering_f_templates = {}
 
-_ans_bidaf_templates = {
-    "greet": [],
-    "goodbye": [],
-    "affirm": [],
-    "deny": [],
-    "exclaim_neg": [],
-    "exclaim_pos": [],
-    "thanks": [],
-    "ynq": [],
-    "whq": [],
-    "idk_response": [],
-    "request_increment": [],
-    "refocus_topic": [],
-    "clarification_request": [],
-    "correction": [],
-    "ask_if_ended": [],
-    "ask_for_story": [],
-    "comment": [],
-    "feedback_prompt": [],
-}
+_ans_bidaf_templates = {}
 
-_link_to_survey_templates = {
-    "greet": [],
-    "goodbye": [],
-    "affirm": [],
-    "deny": [],
-    "exclaim_neg": [],
-    "exclaim_pos": [],
-    "thanks": [],
-    "ynq": [],
-    "whq": [],
-    "idk_response": [],
-    "request_increment": [],
-    "refocus_topic": [],
-    "clarification_request": [],
-    "correction": [],
-    "ask_if_ended": [],
-    "ask_for_story": [],
-    "comment": [],
-    "feedback_prompt": [],
-}
+_link_to_survey_templates = {}
 
 
 class NLG:
@@ -193,86 +89,58 @@ class StorytellerGenerator(NLG):
     def __init__(self, args):
         logger.info("Creating features from dataset file at ")
 
-    # def fill_template(self, state_dic):
-    #     templates = _driver_templates
-    #     template_fillers = {k: v["label"] for k, v in results.items() if type(v) == dict}
-    #     ## here template_fillers should have "entity", "quest", "object"
-    #     # whatever required by your fillers
-    #
-    #     if "greet" in results["intents"] and "introduction" in results["state"]:
-    #         curr_templates = templates["greet"]
-    #         template = random.choice(curr_templates["introduction"])
-    #         return template.format(**template_fillers)
-
     def fill_intro(self, state_dic):
         templates = _intro_templates
         template_fillers = {'text': state_dic["text"]}
-
-        if "greet" in state_dic["intents"]:
-            curr_templates = templates["greet"]
-            template = random.choice(curr_templates)
-            return template
-        elif "request_increment" in state_dic["intents"]:
-            curr_templates = templates["request_increment"]
-            template = random.choice(curr_templates)
-            return template.format(**template_fillers)
-        else:
-            logger.info("Cant generated template for: {}".format(state_dic))
+        no_story = ["greet", "goodbye", "ynq", "whq", "clarification_request",
+                    "ask_if_ended", "feedback_prompt"]
+        with_story = ["affirm", "exclaim_neg", "exclaim_pos", "thanks", "request_increment",
+                      "ask_for_story", "comment"]
+        for intent in no_story:
+            if intent in state_dic["intents"]:
+                curr_templates = templates[intent]
+                template = random.choice(curr_templates)
+                return template
+        for intent2 in with_story:
+            if intent2 in state_dic["intents"]:
+                curr_templates = templates[intent2]
+                template = random.choice(curr_templates)
+                return template.format(**template_fillers)
 
     def fill_storytelling(self, state_dic):
         templates = _storytelling_templates
-        if "greet" in state_dic["intents"]:
-            curr_templates = templates["greet"]
-            template = random.choice(curr_templates)
-            return template
-        else:
-            logger.info("Cant generated template for: {}".format(state_dic))
+        template_fillers = {'text': state_dic["text"]}
+        with_story = ["affirm", "exclaim_neg", "exclaim_pos", "thanks", "request_increment",
+                      "ask_for_story", "comment"]
+        no_story = ["greet", "goodbye", "ynq", "whq", "clarification_request",
+                    "ask_if_ended", "feedback_prompt"]
+        for intent in with_story:
+            if intent in state_dic["intents"]:
+                curr_templates = templates["greet"]
+                template = random.choice(curr_templates)
+                return template.format(**template_fillers)
+        for intent in no_story:
+            if intent in state_dic["intents"]:
+                curr_templates = templates[intent]
+                template = random.choice(curr_templates)
+                return template.format(**template_fillers)
+        # else:
+        #     logger.info("Cant generated template for: {}".format(state_dic))
 
     def fill_closing(self, state_dic):
         templates = _closing_templates
-        if "greet" in state_dic["intents"]:
-            curr_templates = templates["greet"]
-            template = random.choice(curr_templates)
-            return template
-        else:
-            logger.info("Cant generated template for: {}".format(state_dic))
 
     def fill_answering(self, state_dic):
         templates = _answering_templates
-        if "greet" in state_dic["intents"]:
-            curr_templates = templates["greet"]
-            template = random.choice(curr_templates)
-            return template
-        else:
-            logger.info("Cant generated template for: {}".format(state_dic))
 
     def fill_answering_f(self, state_dic):
         templates = _answering_f_templates
-        if "greet" in state_dic["intents"]:
-            curr_templates = templates["greet"]
-            template = random.choice(curr_templates)
-            return template
-        else:
-            logger.info("Cant generated template for: {}".format(state_dic))
 
     def fill_ans_bidaf(self, state_dic):
         templates = _ans_bidaf_templates
-        if "greet" in state_dic["intents"]:
-            curr_templates = templates["greet"]
-            template = random.choice(curr_templates)
-            return template
-        else:
-            logger.info("Cant generated template for: {}".format(state_dic))
 
     def fill_link_to_survey(self, state_dic):
         templates = _link_to_survey_templates
-        if "greet" in state_dic["intents"]:
-            curr_templates = templates["greet"]
-            template = random.choice(curr_templates)
-            return template
-        else:
-            logger.info("Cant generated template for: {}".format(state_dic))
-
 
 
 # if __name__ == "__main__":
