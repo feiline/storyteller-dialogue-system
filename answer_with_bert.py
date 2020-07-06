6780e7462b9340cff82bcbcc1f5287985ad52e0c
feiIin <sabrina.chiesurin@gmail.com>
2020-07-06 17:29:34 +0100

    integration code

--- answer_with_bert.py
+++ answer_with_bert.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import time
 from transformers import BertTokenizer, TFBertForQuestionAnswering
 
 
@@ -9,18 +8,16 @@ def get_bert_model():
 
 
 def get_bert_answer(model, question):
-    text = "I wish I had a digital camera because something really funny happened " \
-           "and I wanted to capture it " \
-           "on camera. We keep a large stainless bowl of water outside the house for Benjamin, our dog, to " \
-             "drink off. His bowl has become a very popular site, there are often other animals to drink there" \
-             "despite being the dog bowl. Throughout the day, many birds drink out of it and bathe in it. " \
-             "The birds literally line up on the railing and wait their turn. Squirrels also go to drink there." \
-             " A squirrel that acted in a very crazy way just came by. He was literally jumping in fright at " \
-             "seeing his own reflection in the bowl, or at least I think so. He was startled so much at one" \
-             " point that he leap in the air and fell off the deck, but no worries, the squirrel did not hurt" \
-             " himself. I saw his one little paw hanging on! After a moment or two his paw slipped and he " \
-             "tumbled down a few feet. It was really fun, you should have seen the look on his startled face" \
-             " and how he jumped back each time he caught his reflection in the bowl!"
+    text = "This is one of those times I wish I had a digital camera. We keep a large stainless bowl of water outside " \
+           "the house for Benjamin to drink off. His bowl has become a very popular site because there are often other " \
+           "animals to drink there despite being the dog bowl. Throughout the day, many birds drink out of it and bathe" \
+           " in it. The birds literally line up on the railing and wait their turn. Squirrels also go to drink there." \
+           " A squirrel that acted in a very crazy way just came by. He was literally jumping in fright at " \
+           "what I believe was his own reflection in the bowl. He was startled so much at one" \
+           " point that he leap in the air and fell off the deck, but no worries, the squirrel did not hurt" \
+           " himself. I saw his one little paw hanging on! After a moment or two his paw slipped and he " \
+           "tumbled down a few feet. It was really fun, you should have seen the look on his startled face" \
+           " and how he jumped back each time he caught his reflection in the bowl!"
 
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     encoding = tokenizer.encode_plus(question, text)
@@ -32,61 +29,48 @@ def get_bert_answer(model, question):
     return answer
 
 
-if __name__ in "__main__":
-    model = get_bert_model()
-    question = "why you wish to have a camera?"
-    answer = get_bert_answer(model, question)
-    print(question, answer)
-    question = "who is benjamin?"
-    answer = get_bert_answer(model, question)
-    print(question, answer)
-    question = "why is the bowl popular?"
-    answer = get_bert_answer(model, question)
-    print(question, answer)
-    question = "was the squirrel hurt?"
-    answer = get_bert_answer(model, question)
-    print(question, answer)
-    question = "why was the squirrel afraid?"
-    answer = get_bert_answer(model, question)
-    print(question, answer)
-    question = "what happened after a few moments?"
-    answer = get_bert_answer(model, question)
-    print(question, answer)
-    question = "what should I have seen?"
-    answer = get_bert_answer(model, question)
-    print(question, answer)
-    question = "was it that nice?"
-    answer = get_bert_answer(model, question)
-    print(question, answer)
-    question = "wait what?"
-    answer = get_bert_answer(model, question)
-    print(question, answer)
-    question = "what?"
-    answer = get_bert_answer(model, question)
-    print(question, answer)
-    question = "where?"
-    answer = get_bert_answer(model, question)
-    print(question, answer)
-    question = "why?"
-    answer = get_bert_answer(model, question)
-    print(question, answer)
-    question = "when?"
-    answer = get_bert_answer(model, question)
-    print(question, answer)
-    question = "what?"
-    answer = get_bert_answer(model, question)
-    print(question, answer)
-
-
-
-# for i in range(0, 100):
-#     start_time = time.time()
-#     question, text = "Who was Jim Henson?", "Jim Henson was a nice puppet"
-#     encoding = tokenizer.encode_plus(question, text)
-#     input_ids, token_type_ids = encoding["input_ids"], encoding["token_type_ids"]
-#
-#
-#     all_tokens = tokenizer.convert_ids_to_tokens(input_ids)
-#     answer = ' '.join(all_tokens[tf.math.argmax(tf.squeeze(start_scores)) : tf.math.argmax(tf.squeeze(end_scores))+1])
-#     print(answer)
-#     print(1 / (time.time() - start_time), "hz")
+# --------------- for DEBUG porpuse only ----------------- #
+# if __name__ in "__main__":
+#     model = get_bert_model()
+#     question = "why do you wish to have a camera?"
+#     answer = get_bert_answer(model, question)
+#     print(question, answer)
+#     question = "who is benjamin?"
+#     answer = get_bert_answer(model, question)
+#     print(question, answer)
+#     question = "why is the bowl popular?"
+#     answer = get_bert_answer(model, question)
+#     print(question, answer)
+#     question = "was the squirrel hurt?"
+#     answer = get_bert_answer(model, question)
+#     print(question, answer)
+#     question = "why was the squirrel afraid?"
+#     answer = get_bert_answer(model, question)
+#     print(question, answer)
+#     question = "what happened after a few moments?"
+#     answer = get_bert_answer(model, question)
+#     print(question, answer)
+#     question = "what should I have seen?"
+#     answer = get_bert_answer(model, question)
+#     print(question, answer)
+#     question = "was it that nice?"
+#     answer = get_bert_answer(model, question)
+#     print(question, answer)
+#     question = "wait what?"
+#     answer = get_bert_answer(model, question)
+#     print(question, answer)
+#     question = "what?"
+#     answer = get_bert_answer(model, question)
+#     print(question, answer)
+#     question = "where?"
+#     answer = get_bert_answer(model, question)
+#     print(question, answer)
+#     question = "why?"
+#     answer = get_bert_answer(model, question)
+#     print(question, answer)
+#     question = "when?"
+#     answer = get_bert_answer(model, question)
+#     print(question, answer)
+#     question = "what?"
+#     answer = get_bert_answer(model, question)
+#     print(question, answer)
